37be073b064425f91510c8d2c35df48b0a5dbe61
Bugra Can Kus <bugracan_kus@hotmail.com>
2023-11-12 17:02:42 +0300

    Initial commit

---
+++ screens/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+const HomePage: React.FC = () => {
+    return (
+        <View style={styles.container}>
+        </View>
+    )
+}
+export default HomePage;
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "blue",
+        width:100, 
+        height:100, 
+    },
+});
